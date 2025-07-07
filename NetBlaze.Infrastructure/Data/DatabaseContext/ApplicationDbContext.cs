6c9b733ff4683ed34ee6fe09a92487ebb4254630
Mostafa Ashraf <mostafaashrafelsersi@gmail.com>
2025-07-07 11:57:45 +0300

    Add project files.

---
+++ NetBlaze.Infrastructure/Data/DatabaseContext/ApplicationDbContext.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using NetBlaze.Domain.Entities;
+using NetBlaze.Domain.Entities.Identity;
+using NetBlaze.Infrastructure.Data.Configurations.MiscConfigurations;
+using System.Reflection;
+
+namespace NetBlaze.Infrastructure.Data.DatabaseContext
+{
+    public class ApplicationDbContext : IdentityDbContext<User,
+                                                          Role,
+                                                          long,
+                                                          IdentityUserClaim<long>,
+                                                          UserRole,
+                                                          IdentityUserLogin<long>,
+                                                          IdentityRoleClaim<long>,
+                                                          IdentityUserToken<long>>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+
+        public DbSet<SampleEntity> Samples => Set<SampleEntity>();
+
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+            builder.ConfigureIdentityTablesNames();
+
+            builder.SetGlobalIsDeletedFilterToAllEntities();
+        }
+    }
+}
